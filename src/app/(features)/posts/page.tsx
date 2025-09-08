bdafb9a2643ea74711213ece1310b16af600b1a2
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-09-08 21:43:29 +0630

    update getAllPosts Method

--- src/app/(features)/posts/page.tsx
+++ src/app/(features)/posts/page.tsx
@@ -1,10 +1,19 @@
 import PostLists from "@/components/features/posts/PostLists";
 import { getAllPosts } from "@/features/posts/helper";
+import { Post } from "@/features/posts/type";
 import { readFile } from "fs";
 import React from "react";
 
 const PostPage = async () => {
-  const posts = await getAllPosts();
+  const [posts, { success, error }] = await getAllPosts();
+
+  if (!success) {
+    return (
+      <>
+        <p className="text-red-600 p-3"> {error as string}</p>
+      </>
+    );
+  }
   return (
     <React.Fragment>
       <main className="w-full min-h-screen container mx-auto">
--- src/features/posts/helper.ts
+++ src/features/posts/helper.ts
@@ -1,7 +1,23 @@
 import { type Post } from "./type";
 
-export async function getAllPosts(): Promise<Post[] | undefined> {
-    const response = await fetch(`https://dummyjson.com/posts`);
-    const { posts }: { posts: Post[] | undefined } = await response.json();
-    return posts;
+export interface ErrorType {
+    success?: boolean;
+    error?: string;
+}
+
+export async function getAllPosts(): Promise<[Post[] | undefined, status: ErrorType]> {
+    try {
+        const response = await fetch(`https://dummyjson.com/posts`);
+        const { posts } = await response.json();
+        if (!posts) {
+            return [undefined, { success: false, error: 'not post found!' }]
+        }
+        return [posts, { success: true, error: undefined }]
+    } catch (error) {
+        if (error instanceof Error) {
+            return [undefined, { success: false, error: error?.message }]
+
+        }
+        return [undefined, { success: false, error: 'unknown error!' }];
+    }
 }
