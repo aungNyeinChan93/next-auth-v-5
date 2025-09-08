9479ebb639dc54dc286d102a6819f3e0c999341d
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-09-08 21:47:32 +0630

    update

--- src/features/posts/helper.ts
+++ src/features/posts/helper.ts
@@ -10,13 +10,12 @@ export async function getAllPosts(): Promise<[Post[] | undefined, status: ErrorT
         const response = await fetch(`https://dummyjson.com/posts`);
         const { posts } = await response.json();
         if (!posts) {
-            return [undefined, { success: false, error: 'not post found!' }]
+            return [undefined, { success: false, error: 'post not found!' }]
         }
         return [posts, { success: true, error: undefined }]
     } catch (error) {
         if (error instanceof Error) {
             return [undefined, { success: false, error: error?.message }]
-
         }
         return [undefined, { success: false, error: 'unknown error!' }];
     }
